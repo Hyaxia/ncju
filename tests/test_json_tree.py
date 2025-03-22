5d61d40612238676e9eb09379e6d32dcb5d32f87
hyaxia <hyaxia@gmail.com>
2025-03-22 16:19:53 +0200

    update tests

--- tests/test_json_tree.py
+++ tests/test_json_tree.py
@@ -4,7 +4,7 @@ import json
 
 
 def test_iterate_json():
-    simple_json_path = '/Users/maximvovshin/software/ncju/tests/test_data/simple_json.json'
+    simple_json_path = './tests/test_data/simple_json.json'
     simple_json_size =  get_size_of_file(simple_json_path)
     assert simple_json_size == 37
 
@@ -13,4 +13,14 @@ def test_iterate_json():
     tree = build_tree(simple_json)
     assert tree.size == simple_json_size
 
+
+def test_nested_json():
+    nested_json_path = './tests/test_data/nested_json.json'
+    nested_json_size =  get_size_of_file(nested_json_path)
+    assert nested_json_size == 86  # this is the current size of the file
+
+    with open(nested_json_path) as f:
+        simple_json = json.load(f)
+    tree = build_tree(simple_json)
+    assert tree.size == 47  # size without indentation and special marks
 
